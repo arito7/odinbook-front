cb41492102c764deb1d244922533cb6d864b84b3
arito7 <64824933+arito7@users.noreply.github.com>
2022-05-04 12:47:51 +0900

    Add search bar and checkout these people card

--- src/pages/friends.jsx
+++ src/pages/friends.jsx
@@ -1,15 +1,22 @@
-import { Message, MessageRounded } from '@mui/icons-material';
+import {
+  Message,
+  MessageRounded,
+  PersonAddAltRounded,
+  SearchRounded,
+} from '@mui/icons-material';
 import {
   Avatar,
   Box,
   Button,
   Divider,
   IconButton,
+  InputAdornment,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
   Paper,
+  TextField,
   Typography,
 } from '@mui/material';
 import React from 'react';
@@ -19,6 +26,47 @@ const friendsSet = [{ username: 'Beast Fiend', icon: '/images/man.png' }];
 const Friends = () => {
   return (
     <Box>
+      <Box sx={{ margin: '1rem' }} s>
+        <TextField
+          fullWidth
+          placeholder="Find friends!"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchRounded />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      <Paper elevation={1} sx={{ margin: '1rem' }}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ padding: '1rem', paddingBottom: 0 }}
+        >
+          Checkout these people!
+        </Typography>
+        <Divider />
+        <List>
+          {friendsSet.map((friend) => (
+            <ListItem
+              secondaryAction={
+                <IconButton color="primary">
+                  <PersonAddAltRounded />
+                </IconButton>
+              }
+            >
+              <ListItemAvatar>
+                <Avatar src={friend.icon} />
+              </ListItemAvatar>
+              <ListItemText primary={friend.username} />
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+
       <Paper elevation={1} sx={{ padding: '1rem', margin: '1rem' }}>
         <Typography variant="h5" gutterBottom>
           Friend Requests
@@ -61,7 +109,7 @@ const Friends = () => {
           {friendsSet.map((friend) => (
             <ListItem
               secondaryAction={
-                <IconButton>
+                <IconButton color="primary">
                   <MessageRounded />
                 </IconButton>
               }
