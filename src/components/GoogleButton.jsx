6c283d1cf74ae6218cd4492cad69771c7395c9a5
arito7 <64824933+arito7@users.noreply.github.com>
2022-05-06 00:04:29 +0900

    Implement google sign in from client

---
+++ src/components/GoogleButton.jsx
@@ -0,0 +1,35 @@
+import { Button } from '@mui/material';
+import { Google as GoogleIcon } from '@mui/icons-material';
+import GoogleLogin from 'react-google-login';
+import { Typography } from '@mui/material';
+
+const GoogleButton = ({ text }) => {
+  const onSignin = (googleId, tokenId, accessToken, tokenObj, profileObj) => {
+    console.log(googleId, tokenId, accessToken, tokenObj, profileObj);
+  };
+
+  const onFailure = (err, details) => {
+    console.log(err, details);
+  };
+
+  return (
+    <GoogleLogin
+      render={(renderProps) => (
+        <Button
+          onClick={renderProps.onClick}
+          className="text-btn"
+          variant="contained"
+          sx={{ padding: '.5rem' }}
+        >
+          <GoogleIcon sx={{ mr: '1rem' }} />
+          <Typography variant="subtitle2">{text}</Typography>
+        </Button>
+      )}
+      onSuccess={onSignin}
+      onFailure={onFailure}
+      clientId="354732009283-p8t311qroram713iu2jn90trpoe7vvp5.apps.googleusercontent.com"
+    />
+  );
+};
+
+export default GoogleButton;
