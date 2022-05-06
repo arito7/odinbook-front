4f77284dc4ff57c830ed1309947b5ecfcfe07f59
arito7 <64824933+arito7@users.noreply.github.com>
2022-05-07 01:24:07 +0900

    Add bearer token to each request

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,8 +1,10 @@
 import Axios from 'axios';
+import local from '../helpers/localStorage';
 
 const axios = Axios.create({
   baseURL: 'http://localhost:5000',
   timeout: 1000,
+  headers: { Authorization: `Bearer ${local.getJwt()}` },
 });
 
 export default axios;
