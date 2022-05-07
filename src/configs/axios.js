51374cc8876fb3e470f704accff0b7ee933bc6e7
arito7 <64824933+arito7@users.noreply.github.com>
2022-05-07 13:35:32 +0900

    Export a new instance each call to ensure newest jwt

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,10 +1,8 @@
 import Axios from 'axios';
 import local from '../helpers/localStorage';
 
-const axios = Axios.create({
+export default Axios.create({
   baseURL: 'http://localhost:5000',
   timeout: 1000,
   headers: { Authorization: `Bearer ${local.getJwt()}` },
 });
-
-export default axios;
