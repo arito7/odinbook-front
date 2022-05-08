b69cd2d5b8d4f9dfd6ebe5ad63a666a817f85d02
Yuji T <accmanager1990@gmail.com>
2022-05-09 01:03:57 +0900

    Implement useFriendRequests hook

--- src/components/FriendRequests.jsx
+++ src/components/FriendRequests.jsx
@@ -1,13 +1,9 @@
 import { Avatar, Box, Button, Divider, Paper, Typography } from '@mui/material';
 import React from 'react';
-import { useAuth } from '../contexts/AuthContext';
+import { useFriendRequests } from '../hooks/useFriendRequests';
 
-/**
- * handle no friends
- * handle no requests
- */
 export const FriendRequests = () => {
-  const auth = useAuth();
+  const [requests, pendingRequests] = useFriendRequests();
 
   return (
     <Paper elevation={1} sx={{ padding: '1rem', margin: '1rem' }}>
@@ -15,34 +11,68 @@ export const FriendRequests = () => {
         Friend Requests
       </Typography>
       <Divider sx={{ margin: '.5rem -1rem' }} />
-      {auth.user.friendRequests.map((req) => (
-        <Box
-          key={req._id}
-          sx={{
-            display: 'grid',
-            gap: '.5rem',
-            gridTemplateColumns: 'auto 1fr',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar src={req.icon} />
-          <Box>
-            <Typography gutterBottom>{req.username}</Typography>
-            <Box>
-              <Button
-                size="small"
-                sx={{ marginRight: '.5rem' }}
-                variant="contained"
-              >
-                Confirm
-              </Button>
-              <Button size="small" variant="outlined" color="error">
-                Decline
-              </Button>
+      {requests.length ? (
+        <>
+          {requests.map((req) => (
+            <Box
+              key={req._id}
+              sx={{
+                display: 'grid',
+                gap: '.5rem',
+                gridTemplateColumns: 'auto 1fr',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar src={req.iconUrl || '/images/man.png'} />
+              <Box>
+                <Typography gutterBottom>{req.username}</Typography>
+                <Box>
+                  <Button
+                    size="small"
+                    sx={{ marginRight: '.5rem' }}
+                    variant="contained"
+                  >
+                    Confirm
+                  </Button>
+                  <Button size="small" variant="outlined" color="error">
+                    Decline
+                  </Button>
+                </Box>
+              </Box>
             </Box>
-          </Box>
-        </Box>
-      ))}
+          ))}
+        </>
+      ) : (
+        <Typography sx={{ textAlign: 'center', paddingTop: '1rem' }}>
+          You don't have any friend requests yet :(
+        </Typography>
+      )}
+
+      {pendingRequests.length ? (
+        <>
+          <Typography variant="h5" gutterBottom>
+            Pending Requests
+          </Typography>
+
+          <Divider sx={{ margin: '.5rem -1rem' }} />
+          {pendingRequests.map((req) => (
+            <Box
+              key={req._id}
+              sx={{
+                display: 'grid',
+                gap: '.5rem',
+                gridTemplateColumns: 'auto 1fr',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar src={req.iconUrl || '/images/man.png'} />
+              <Box>
+                <Typography gutterBottom>{req.username}</Typography>
+              </Box>
+            </Box>
+          ))}
+        </>
+      ) : null}
     </Paper>
   );
 };
