c373419704f882269978e193317e227d320a5944
Yuji T <accmanager1990@gmail.com>
2022-05-09 01:45:36 +0900

    Add SnackbarProvider

---
+++ src/contexts/SnackbarContext.js
@@ -0,0 +1,46 @@
+import { Alert, Snackbar } from '@mui/material';
+import React, { createContext, useContext, useState } from 'react';
+
+const SnackbarContext = createContext(null);
+
+export function SnackbarProvider({ children }) {
+  const [message, setMessage] = useState('');
+  const [severity, setSeverity] = useState('success');
+  const [open, setOpen] = useState(false);
+
+  const onClose = (reason) => {
+    if (reason === 'clickaway') return;
+    setOpen(false);
+  };
+
+  const show = (
+    message = 'You need to set a message',
+    severity = 'success'
+  ) => {
+    setMessage(message);
+    setSeverity(severity);
+    setOpen(true);
+  };
+
+  const value = { setMessage, setSeverity, setOpen, show };
+
+  return (
+    <SnackbarContext.Provider value={value}>
+      <Snackbar
+        autoHideDuration={2000}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        open={open}
+        onClose={onClose}
+      >
+        <Alert severity={severity} onClose={onClose}>
+          {message}
+        </Alert>
+      </Snackbar>
+      {children}
+    </SnackbarContext.Provider>
+  );
+}
+
+export function useSnackbar() {
+  return useContext(SnackbarContext);
+}
